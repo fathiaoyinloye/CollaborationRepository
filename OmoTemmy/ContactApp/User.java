7092999b43f85f88e3980cfa5d2c48bf2b667b96
fathiaoyinloye <fathiaoyinloye20@gmail.com>
2026-02-22 21:02:10 +0100

    Omotemmy working...

--- OmoTemmy/ContactApp/User.java
+++ OmoTemmy/ContactApp/User.java
@@ -35,20 +35,41 @@ public class User {
         if(input.isEmpty()) throw new EmptyInputException();
     }
     private void validateTelephone(String telephone){
-        if(findContact(telephone) != null) throw new ContactAlreadyExistException();
+        if(findContactByPhonNumber(telephone) != null) throw new ContactAlreadyExistException();
     }
 
 
     public  void removeContact(String telephone){
-        Contact contact = findContact(telephone);
+        Contact contact = findContactByPhonNumber(telephone);
         if (contacts != null)contacts.remove(contact);
     }
 
-    public Contact findContact(String telephone){
+    public Contact findContactByPhonNumber(String telephone){
         for(Contact contact : contacts){
             if (contact.getTelephone().equals(telephone)) return contact;
         }
         return  null;
 
     }
+
+    public Contact findContactByName(String name){
+        for(Contact contact : contacts){
+            if (contact.getTelephone().equals(name)) return contact;
+        }
+        return  null;
+
+    }
+
+    public Contact editContact(String phonenumber, String newPhoneNumber){
+        Contact contact = findContactByPhonNumber(phonenumber);
+        if (contacts != null){
+            contacts.remove(contact);
+            contact.setTelephone(newPhoneNumber);
+            contacts.add(contact);
+            return contact;
+        }
+        return null;
+    }
+
+
 }
